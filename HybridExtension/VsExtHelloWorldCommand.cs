d9261020c4be3ba562194eb2ba15f2e86011f704
Christopher Christou <chrstou@amazon.com>
2024-03-22 14:16:02 -0700

    Add in VisualStudio.Extensibility and a sample Command

---
+++ HybridExtension/VsExtHelloWorldCommand.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+using Microsoft.VisualStudio.Extensibility.Shell;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HybridExtension
+{
+    /// <summary>
+    /// Command1 handler.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class VsExtHelloWorldCommand : Command
+    {
+        private readonly TraceSource logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VsExtHelloWorldCommand"/> class.
+        /// </summary>
+        /// <param name="traceSource">Trace source instance to utilize.</param>
+        public VsExtHelloWorldCommand(TraceSource traceSource)
+        {
+            // This optional TraceSource can be used for logging in the command. You can use dependency injection to access
+            // other services here as well.
+            this.logger = Requires.NotNull(traceSource, nameof(traceSource));
+        }
+
+        /// <inheritdoc />
+        //public override CommandConfiguration CommandConfiguration => new("%VsSdkPackage.HelloWorldCommand.DisplayName%")
+        public override CommandConfiguration CommandConfiguration => new("%HybridExtension.VsExtHelloWorldCommand.DisplayName%")
+        {
+            // Use this object initializer to set optional parameters for the command. The required parameter,
+            // displayName, is set above. DisplayName is localized and references an entry in .vsextension\string-resources.json.
+            Icon = new(ImageMoniker.KnownValues.Extension, IconSettings.IconAndText),
+            Placements = new[] { CommandPlacement.KnownPlacements.ExtensionsMenu },
+        };
+
+        /// <inheritdoc />
+        public override Task InitializeAsync(CancellationToken cancellationToken)
+        {
+            // Use InitializeAsync for any one-time setup or initialization.
+            return base.InitializeAsync(cancellationToken);
+        }
+
+        /// <inheritdoc />
+        public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
+        {
+            await this.Extensibility.Shell().ShowPromptAsync("Hello from VS.Ext extension contribution!", PromptOptions.OK, cancellationToken);
+        }
+    }
+}
